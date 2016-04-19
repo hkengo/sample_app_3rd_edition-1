03bff95cf499657d92ea0a80cf0212de9fc6fd80
hayashi_kengo <hayashikengojp@gmeil.com>
2016-04-19 20:11:12 +0900

    Add replay micropost to home page

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,9 +5,25 @@ class Micropost < ActiveRecord::Base
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   validate  :picture_size
-  
+
+  #リプライのmicropostの場合true，そうでない場合falseを返す
+  def is_replay_micropost?
+   !to_micropost_id.nil?
+  end
+
+  #リプライされている場合true, そうでない場合falseを返す
+  def is_replayed?
+    !!Micropost.find_by(to_micropost_id: id)
+  end
+
+  #自分をreplayしているmicropostを探す
+  def find_replay
+    Micropost.where(to_micropost_id: id)
+
+  end
+
   private
-  
+
     # Validates the size of an uploaded picture.
     def picture_size
       if picture.size > 5.megabytes
