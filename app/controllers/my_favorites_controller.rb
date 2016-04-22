4eacc79ab2756a1801ce79638a6e005992ceb6ac
hayashi_kengo <hayashikengojp@gmeil.com>
2016-04-22 20:03:23 +0900

    Improve review point

--- app/controllers/my_favorites_controller.rb
+++ app/controllers/my_favorites_controller.rb
@@ -2,7 +2,6 @@ class MyFavoritesController < ApplicationController
 	
 	def create
 		@micropost = Micropost.find(params[:micropost_id])
-		@user = current_user
 		current_user.favorite(@micropost)
 		respond_to do |format|
 			format.html { redirect_to root_url }
@@ -12,7 +11,6 @@ class MyFavoritesController < ApplicationController
 	
 	def destroy
 		@micropost = Micropost.find(params[:micropost_id])
-		@user = current_user
 		current_user.unfavorite(@micropost)
 		respond_to do |format|
 			format.html { redirect_to root_url }
